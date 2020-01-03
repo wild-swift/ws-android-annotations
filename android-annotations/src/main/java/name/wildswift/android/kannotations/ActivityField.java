1547fb831dfc903ef2990af93e62ab2b64012282
swift <www.swift.ru@gmail.com>
2020-01-03 19:35:00 +0300

    update library versions + update copyright

--- android-annotations/src/main/java/name/wildswift/android/kannotations/ActivityField.java
+++ android-annotations/src/main/java/name/wildswift/android/kannotations/ActivityField.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2018 Wild Swift
+ * Copyright (C) 2020 Wild Swift
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,10 +14,6 @@
  * limitations under the License.
  */
 package name.wildswift.android.kannotations;
-
-/**
- * Created by swift
- */
 
 public @interface ActivityField {
     String name();
