b591533a754b9251749f5f6ab5327c3cf3ff25fa
swift <www.swift.ru@gmail.com>
2020-04-23 22:45:22 +0300

    Fix docs

--- android-annotations/src/main/java/name/wildswift/android/kannotations/CollectionViewField.java
+++ android-annotations/src/main/java/name/wildswift/android/kannotations/CollectionViewField.java
@@ -44,4 +44,6 @@ public @interface CollectionViewField {
     String modelFieldName() default "viewModel";
     String modelSetterName() default "";
 
+    ViewEvent[] elementEvents() default {};
+
 }
